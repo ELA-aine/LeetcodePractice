b5e7314c959a98258ac2c7474cc0a0998e42d220
ELA-aine <54710511+ELA-aine@users.noreply.github.com>
2024-10-30 20:34:47 -0400

    new file

---
+++ coursera_practice/ex1.py
@@ -0,0 +1,30 @@
+#ex 1: print value
+print('this is my first program')
+
+#ex 2: set variable and print
+myint = 200
+print(myint)
+
+#ex 3: perform simple arithmetic operations
+a = 20
+c = a * 100
+print(c)
+
+#ex 4: execute conditional statements
+myval = 8
+
+if myval > 0:
+    print('It is a positive number')
+else:
+    print('it is a negative number')
+
+#ex 5 + 6: declare and print an array using a for loop and use break statement
+myArray = [20, 10, 44, 928, 200]
+
+for i in myArray:
+    if i == 44:
+        break
+    else:
+        print(i)
+
+
